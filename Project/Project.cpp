6a1913179c69a823568ff42793aacff2326159c8
Yun Jang <jang.yunj7@gmail.com>
2023-03-11 23:27:46 -0500

    second commit

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -2,12 +2,22 @@
 
 using namespace std;
 
-int main() {
-	cout << "Enter values for x, y: ";
-	int x;
-	int y;
-	cin >> x >> y;
-	cout << x + y;
+//int main() {
+//	cout << "Enter values for x, y: ";
+//	int x;
+//	int y;
+//	cin >> x >> y;
+//	cout << x + y;
+//
+//	return 0;
+//}
 
-	return 0;
+int main() {
+	cout << "Fahrenheit: ";
+	int fahrenheit;
+	cin >> fahrenheit;
+
+	double celcius = (fahrenheit - 32) / 1.8;
+	cout << celcius;
+
 }
